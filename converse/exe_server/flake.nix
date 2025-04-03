3ed9fcf978911eaadd8ee8894254ae99f49364a2
dseum <dseum.me@gmail.com>
2025-04-03 00:36:44 -0400

    ppush

--- converse/exe_server/flake.nix
+++ converse/exe_server/flake.nix
@@ -1,36 +1,47 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
   outputs =
-    { self, nixpkgs }:
-    let
-      system = "aarch64-darwin";
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          apple-sdk_15
-        ];
-        nativeBuildInputs = with pkgs; [
-          autoconf
-          autoconf-archive
-          automake
-          cmake
-          git
-          llvmPackages_20.clang-tools
-          llvmPackages_20.clangWithLibcAndBasicRtAndLibcxx
-          ninja
-          pkg-config
-          vcpkg
-        ];
-        shellHook = ''
-          export VCPKG_ROOT=${pkgs.vcpkg}/share/vcpkg
-          export VCPKG_FORCE_SYSTEM_BINARIES=1
-        '';
-      };
-    };
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachSystem [ "aarch64-darwin" "x86_64-linux" ] (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs =
+            with pkgs;
+            [
+              (if pkgs.stdenv.isDarwin then apple-sdk_15 else null)
+            ]
+            ++ (with pkgs; [
+              autoconf
+              autoconf-archive
+              automake
+              cmake
+              git
+              llvmPackages_20.clang-tools
+              llvmPackages_20.clangWithLibcAndBasicRtAndLibcxx
+              ninja
+              pkg-config
+              vcpkg
+            ]);
+
+          shellHook = ''
+            export VCPKG_ROOT=${pkgs.vcpkg}/share/vcpkg
+            export VCPKG_FORCE_SYSTEM_BINARIES=1
+          '';
+        };
+      }
+    );
 }
