c5fe66acf3aced73c354b52a34bd785fa3fcd264
dseum <dseum.me@gmail.com>
2025-04-03 01:30:34 -0400

    aarch64 linux

--- converse/exe_server/flake.nix
+++ converse/exe_server/flake.nix
@@ -10,38 +10,44 @@
       nixpkgs,
       flake-utils,
     }:
-    flake-utils.lib.eachSystem [ "aarch64-darwin" "x86_64-linux" ] (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs =
-            with pkgs;
-            [
-              (if pkgs.stdenv.isDarwin then apple-sdk_15 else null)
-            ]
-            ++ (with pkgs; [
-              autoconf
-              autoconf-archive
-              automake
-              cmake
-              git
-              llvmPackages_20.clang-tools
-              llvmPackages_20.clangWithLibcAndBasicRtAndLibcxx
-              ninja
-              pkg-config
-              vcpkg
-            ]);
+    flake-utils.lib.eachSystem
+      [
+        "aarch64-darwin"
+        "x86_64-linux"
+        "aarch64-linux"
+      ]
+      (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs =
+              with pkgs;
+              [
+                (if pkgs.stdenv.isDarwin then apple-sdk_15 else null)
+              ]
+              ++ (with pkgs; [
+                autoconf
+                autoconf-archive
+                automake
+                cmake
+                git
+                llvmPackages_20.clang-tools
+                llvmPackages_20.clangWithLibcAndBasicRtAndLibcxx
+                ninja
+                pkg-config
+                vcpkg
+              ]);
 
-          shellHook = ''
-            export VCPKG_ROOT=${pkgs.vcpkg}/share/vcpkg
-            export VCPKG_FORCE_SYSTEM_BINARIES=1
-          '';
-        };
-      }
-    );
+            shellHook = ''
+              export VCPKG_ROOT=${pkgs.vcpkg}/share/vcpkg
+              export VCPKG_FORCE_SYSTEM_BINARIES=1
+            '';
+          };
+        }
+      );
 }
